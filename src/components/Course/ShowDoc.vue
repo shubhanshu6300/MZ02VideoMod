d7b67780bc29bcb371dfbb74db2aa063455c4313
Rishabh Jain <rishabh@jnexsoft.com>
2018-06-25 03:29:21 +0530

    show doc component

---
+++ src/components/Course/ShowDoc.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ doc }}
+  </div>
+</template>
+
+<script>
+
+import { mapGetters } from 'vuex';
+import { store } from '../../store'
+
+export default {
+  name: 'ShowDoc',
+  computed: mapGetters({ doc: 'activeDoc' })
+};
+
+</script>
