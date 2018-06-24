63572db975fe3a80f3dbdf4b68fcde8d3c4b6fca
Rishabh Jain <rishabh@jnexsoft.com>
2018-06-25 03:28:55 +0530

    added store module for couse

--- src/store/index.js
+++ src/store/index.js
@@ -6,11 +6,14 @@ import gen from './modules/gen'
 // training
 import training from './modules/course/training'
 
+import course from './modules/course';
+
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   modules: {
     gen,
-    training
+    training,
+    course
   }
 })
