2b4246188a003e0671eb9a62eda061e833a410bd
Rishabh Jain <rishabh@jnexsoft.com>
2018-06-25 03:29:35 +0530

    wrapper component for docs

---
+++ src/components/Course/ShowDocWrapper.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <div class="col-md-8">
+      <ShowDoc></ShowDoc>
+    </div>
+
+    <div class="col-md-4">
+      <CourseList></CourseList>
+    </div>
+  </div>
+</template>
+
+<script>
+import ShowDoc from './ShowDoc';
+import CourseList from './CourseList';
+import { store } from '../../store'
+
+export default {
+  name: 'ShowDocWrapper',
+  store,
+  components: { ShowDoc, CourseList }
+};
+</script>
